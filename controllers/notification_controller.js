92633094cbb77d5602df66af84b2ffb1ad384654
MD Mizanur Rahaman <codepadding@gmail.com>
2024-04-05 14:42:20 +0400

    post like added

--- controllers/notification_controller.js
+++ controllers/notification_controller.js
@@ -1,12 +1,21 @@
 const { isValidObjectId } = require("mongoose");
+const Likes = require("../models/likes");
 
 module.exports.handleChatConnection = (io) => {
   io.on("connection", (socket) => {
     socket.on("like_post", async (data) => {
       try {
-        const { senderId } = data;
+        const { userId, senderId, itemId } = data;
         console.log(senderId);
         io.emit("post_liked", data);
+
+        const like = new Likes({
+          userId: userId,
+          senderId: senderId,
+          itemId: itemId,
+          addedAt: Date.now(),
+        });
+        await like.save();
       } catch (error) {
         console.error("Error sending chat message:", error);
         socket.emit("error", "Something went wrong");
